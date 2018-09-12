fe624c67b41ea48add69958295050d92a69a5b76
Erick Medina <erickmedinamoreno@hotmail.com>
2018-09-12 11:15:20 +0200

    Initial Commit

---
+++ virtualtourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  virtualtourist
+//
+//  Created by Erick Medina on 12/9/18.
+//  Copyright © 2018 Erick Medina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
